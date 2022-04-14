8b373e1dd7e9f5e91647d91baaba59c7ca67e3d9
Lucas <luks2k19@gmail.com>
2022-04-14 16:53:36 -0300

    new

--- TP1/src/TP1.c
+++ TP1/src/TP1.c
@@ -16,20 +16,32 @@ int main(void) {
 
 	setbuf(stdout, NULL);
 
-	int kilometros;
+	float kilometros;
 	int opcion;
-	int precioAerolineas;
+	float precioVueloAerolineas;
 	float aerolineasCredito;
 	float aerolineasDebito;
 	float aerolineasBitcoin;
 	float aerolineasUnitario;
-	int precioLatam;
+	float precioVueloLatam;
 	float latamCredito;
 	float latamDebito;
 	float latamBitcoin;
 	float latamUnitario;
+	char aerolineasOLatam;
+	int kilometrosForzado;
+	int precioAerolineaForzado;
+	int precioLatamForzado;
+	float diferenciaPrecios;
 
-	while (opcion != 6) {
+	kilometros = 0;
+	precioVueloAerolineas = 0;
+	precioVueloLatam = 0;
+	kilometrosForzado = 7090;
+	precioAerolineaForzado = 162965;
+	precioLatamForzado = 159339;
+
+	do{
 	printf("\n1. Ingresar kilometros"
 	"\n2. Ingresar precio de vuelos"
 	"\n3. Calcular todos los costos"
@@ -38,37 +50,69 @@ int main(void) {
 	"\n6. Salir\n"
 
 	"\nSeleccione una opcion:");
+	fflush(stdin);
 	scanf("%d", &opcion);
 
 	switch(opcion){
 	case 1:
 
-		printf("\nIngrese la cantidad de kilometros:");
-		scanf("%d",&kilometros);
-
+		do {
+			printf("\nIngrese la cantidad de kilometros:");
+			fflush(stdin);
+			scanf("%f",&kilometros);
+		} while (kilometros <= 0);
 		break;
 	case 2:
 
-		printf("\nIngrese el costo del vuelo con Aerolineas: ");
-		scanf("%d",&precioAerolineas);
+		do {
+			printf("\nDesea ingresar el precio de Aerolineas o Latam?"
+					"\n'a' para aerolineas o 'l' para latam) ");
+			fflush(stdin);
+			scanf("%c",&aerolineasOLatam);
 
-		printf("\nIngrese el costo del vuelo con Latam: ");
-		scanf("%d",&precioLatam);
-
+			switch(aerolineasOLatam){
+			case 'a':
+				do {
+					printf("Ingrese el precio del vuelo: ");
+					fflush(stdin);
+					scanf("%f",&precioVueloAerolineas);
+				}while (precioVueloAerolineas <= 0);
+				break;
+			case 'l':
+				do {
+					printf("Ingrese el precio del vuelo: ");
+					fflush(stdin);
+					scanf("%f",&precioVueloLatam);
+				}while (precioVueloLatam <= 0);
+				break;
+			}
+		}while (aerolineasOLatam != 'a' && aerolineasOLatam !='l');
 		break;
 	case 3:
-
+		if (kilometros > 0 && (precioVueloAerolineas > 0 || precioVueloLatam >0 )){
+			calcularValores (precioVueloAerolineas, kilometros, &aerolineasDebito, &aerolineasCredito, &aerolineasBitcoin, &aerolineasUnitario);
+			calcularValores (precioVueloLatam, kilometros, &latamDebito, &latamCredito, &latamBitcoin, &latamUnitario);
+			diferenciaPrecios = calcularDiferencia(precioVueloAerolineas, precioVueloLatam);
+			printf("\nYa estan realizadas las cuentas\n");
+		}else {
+			printf("\nNo ingreso los kilometros o el precio del vuelo\n");
+		}
 		break;
 	case 4:
-
+		mostrarInformacion (precioVueloAerolineas, kilometros, aerolineasDebito, aerolineasCredito, aerolineasBitcoin, aerolineasUnitario, precioVueloLatam, latamDebito, latamCredito, latamBitcoin, latamUnitario, diferenciaPrecios);
 		break;
 	case 5:
+		calcularValores (precioAerolineaForzado, kilometrosForzado, &aerolineasDebito, &aerolineasCredito, &aerolineasBitcoin, &aerolineasUnitario);
+		calcularValores (precioLatamForzado, kilometrosForzado, &latamDebito, &latamCredito, &latamBitcoin, &latamUnitario);
+		diferenciaPrecios = calcularDiferencia(precioAerolineaForzado, precioLatamForzado);
+
+		mostrarInformacion (precioAerolineaForzado, kilometrosForzado, aerolineasDebito, aerolineasCredito, aerolineasBitcoin, aerolineasUnitario, precioLatamForzado, latamDebito, latamCredito, latamBitcoin, latamUnitario, diferenciaPrecios);
 
 		break;
 	case 6:
 
 		break;
 	}
-	}
+	} while (opcion != 6 ) ;
 return EXIT_SUCCESS;
 }
--- TP1/src/calculos.h
+++ TP1/src/calculos.h
@@ -8,6 +8,8 @@
 #ifndef CALCULOS_H_
 #define CALCULOS_H_
 
-
+void calcularValores (float precioVuelo, float kilometros, float* tarjetaDebito, float* tarjetaCredito, float* bitcoin, float* precioUnitario);
+float calcularDiferencia(float precioAerolineas, float precioLatam);
+void mostrarInformacion(float precioAerolineas, float kilometros, float aerolineasDebito, float aerolineasCredito, float aerolineasBitcoin, float aerolineasUnitario,float precioLatam, float latamDebito, float latamCredito, float latamBitcoin, float latamUnitario, float diferenciaPrecios);
 
 #endif /* CALCULOS_H_ */
