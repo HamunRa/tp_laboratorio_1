b72c91fecde010b9346cf590b23ba5509deaccf3
Lucas <luks2k19@gmail.com>
2022-04-16 15:44:07 -0300

    new

--- TP1/src/calculos.h
+++ TP1/src/calculos.h
@@ -8,24 +8,40 @@
 #ifndef CALCULOS_H_
 #define CALCULOS_H_
 
-/// @fn void calcularValores (float precioVuelo, float kilometros, float* tarjetaDebito, float* tarjetaCredito, float* bitcoin, float* precioUnitario)
-/// @brief muestra las opciones
-///
-/// @param opcion
-/// @return retorna la opcion seleccionada
+
+/// @fn void calcularValores (float, float, float*, float*, float*, float*)
+/// @brief
+/// @param precioVuelo
+/// @param kilometros
+/// @param tarjetaDebito
+/// @param tarjetaCredito
+/// @param bitcoin
+/// @param precioUnitario
 void calcularValores (float precioVuelo, float kilometros, float* tarjetaDebito, float* tarjetaCredito, float* bitcoin, float* precioUnitario);
+
+
 /// @fn float calcularDiferencia(float, float)
-/// @brief se encarga de calcular la diferencia entre el precio de aerolineas y el de latam
-///
+/// @brief Se encarga de calcular la diferencia entre dos precios y devuelve el valor.
 /// @param precioAerolineas
 ///	@param precioLatam
-/// @return retorna la opcion seleccionada
+/// @return la diferencia entre los dos numeros
 float calcularDiferencia(float precioAerolineas, float precioLatam);
-/// @fn int Opciones(int)
-/// @brief muestra las opciones
-///
-/// @param opcion
-/// @return retorna la opcion seleccionada
+
+
+/// @fn void mostrarInformacion(float, float, float, float, float, float, float, float, float, float, float, float)
+/// @brief Se encarga de mostrar los datos ya calculados, para que el usuario lo pueda visualizar y avisa si no se ingreso ningun valor
+/// @param precioAerolineas
+/// @param kilometros
+/// @param aerolineasDebito
+/// @param aerolineasCredito
+/// @param aerolineasBitcoin
+/// @param aerolineasUnitario
+/// @param precioLatam
+/// @param latamDebito
+/// @param latamCredito
+/// @param latamBitcoin
+/// @param latamUnitario
+/// @param diferenciaPrecios
 void mostrarInformacion(float precioAerolineas, float kilometros, float aerolineasDebito, float aerolineasCredito, float aerolineasBitcoin, float aerolineasUnitario,float precioLatam, float latamDebito, float latamCredito, float latamBitcoin, float latamUnitario, float diferenciaPrecios);
 
 #endif /* CALCULOS_H_ */
